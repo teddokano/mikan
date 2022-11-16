24a6c8c87213b55c8deb4b89e33d10caa3b1f58f
Tedd OKANO <okano.akifumi@gmail.com>
2022-11-17 06:55:44 +0900

    tile open adjustment

--- remote_demo/demo_lib/DEMO.js
+++ remote_demo/demo_lib/DEMO.js
@@ -6,17 +6,27 @@ function busReset( flag ) {
 }
 
 function allLinkOpen( list ) {
-	s_w	= window.screen.width;
-	s_h	= window.screen.height;
-	count	= list.length;
-	width	= window.screen.width / (count / 2);
-	height	= s_h / 2;
 	
-	console.log( 'window.screen.width  ' + window.screen.width );
-	console.log( 'window.screen.height ' + window.screen.height );
+	const	WINDOW_WIDTH_MIN	= 800;
+	const	WINDOW_HEIGHT_MIN	= 1000;
+	const	count	= list.length;
+	const	n_col	= parseInt( window.screen.width / WINDOW_WIDTH_MIN );
+	const	n_row	= parseInt( (count + (n_col - 1)) / n_col );
 
-//	for ( let url of list ) {
+	let		left_ofst	= window.screen.width / n_col;
+	let		top_ofst	= window.screen.height / n_row;
+
+	width	= (left_ofst < WINDOW_WIDTH_MIN ) ? WINDOW_WIDTH_MIN  : left_ofst;
+	//height	= (top_ofst  < WINDOW_HEIGHT_MIN) ? WINDOW_HEIGHT_MIN : top_ofst;
+	height	= top_ofst;
+	
 	list.forEach( ( url, i ) => {
-		window.open( url, url, 'width=' + width + ',height=' + height + ',left=' + width * (i % parseInt(count / 2)) + ',top=' + height * parseInt(i / (count / 2)) );
+		window.open(	url, 
+						url, 
+						'width=' + width 
+							+ ',height=' + height 
+							+ ',left=' + left_ofst * (i % parseInt((count + (n_row - 1)) / n_row)) 
+							+ ',top=' + top_ofst * parseInt(i / parseInt((count + (n_row - 1)) / n_row))
+					);
 	});
 }
