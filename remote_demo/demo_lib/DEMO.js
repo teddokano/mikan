3e13be1b530902672cea0453958d5205e1706d17
Tedd OKANO <okano.akifumi@gmail.com>
2022-11-17 04:25:02 +0900

    tile open for "open all links

--- remote_demo/demo_lib/DEMO.js
+++ remote_demo/demo_lib/DEMO.js
@@ -6,7 +6,17 @@ function busReset( flag ) {
 }
 
 function allLinkOpen( list ) {
-	for ( let url of list ) {
-		window.open( url );
-	}
+	s_w	= window.screen.width;
+	s_h	= window.screen.height;
+	count	= list.length;
+	width	= window.screen.width / (count / 2);
+	height	= s_h / 2;
+	
+	console.log( 'window.screen.width  ' + window.screen.width );
+	console.log( 'window.screen.height ' + window.screen.height );
+
+//	for ( let url of list ) {
+	list.forEach( ( url, i ) => {
+		window.open( url, url, 'width=' + width + ',height=' + height + ',left=' + width * (i % parseInt(count / 2)) + ',top=' + height * parseInt(i / (count / 2)) );
+	});
 }
