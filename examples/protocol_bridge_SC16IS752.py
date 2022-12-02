c539c5faf082d627f7e212b9567ed1f8f56bc8d1
Tedd OKANO <okano.akifumi@gmail.com>
2022-12-03 05:52:58 +0900

    channel independent instance and made "send()" method for int, list and str

--- examples/protocol_bridge_SC16IS752.py
+++ examples/protocol_bridge_SC16IS752.py
@@ -31,46 +31,95 @@ class SC16IS752( SPI_target ):
 	XOFF1	= 0x06
 	XOFF2	= 0x07
 
-	def __init__( self, interface, cs = 0 ):
+	def __init__( self, interface, cs = 0, channel = 0, osc = 14746500, baud = 9600 ):
 		SPI_target.__init__( self, interface, cs )
 
-		self.reg_access( self.LCR, 0, 0x80 );	# 0x80 to program baud rate
-		self.reg_access( self.DLL, 0, 0x60 );
-		self.reg_access( self.DLH, 0, 0x00 );
-		print( self.reg_access( self.LCR, 0 ) )
-		print( self.reg_access( self.DLL, 0 ) )
-		print( self.reg_access( self.DLH, 0 ) )
+		self.osc	= osc
+		self.ch		= channel
+		self.baud( baud )
 
+		self.reg_access( self.LCR, 0xBF )
+		self.reg_access( self.EFR, 0x10 )	# enable enhanced functions
 
-		self.reg_access( self.LCR, 0, 0xBF );
-		self.reg_access( self.EFR, 0, 0x10 );	# enable enhanced functions
+		self.reg_access( self.LCR, 0x03 )	# 8 data bit, 1 stop bit, no parity
+		self.reg_access( self.FCR, 0x06 )	# reset TXFIFO, reset RXFIFO, non FIFO mode
+		self.reg_access( self.FCR, 0x01 )	# enable FIFO mode
 
-		self.reg_access( self.LCR, 0, 0x03 );	# 8 data bit, 1 stop bit, no parity
-		self.reg_access( self.FCR, 0, 0x06 );	# reset TXFIFO, reset RXFIFO, non FIFO mode
-		self.reg_access( self.FCR, 0, 0x01 );	# enable FIFO mode
+		print( "MCR:0x{:02X}".format( self.reg_access( self.MCR ) ) )
+		print( "LSR:0x{:02X}".format( self.reg_access( self.LSR ) ) )
+
+	def baud( self, baud ):
+		lcr	= self.reg_access( self.LCR )
+		divisor	= int( self.osc / (baud * 16) )
+		print( divisor )
+
+		self.reg_access( self.LCR, 0x80 )	# 0x80 to program baud rate
+		self.reg_access( self.DLL, divisor & 0xFF )
+		self.reg_access( self.DLH, divisor >> 8 )
+		
+		print( divisor )
+
+		self.reg_access( self.LCR, lcr )
 		
 	def reg_access( self, *args ):
 		n_args	= len( args )
-		if n_args is 2:
-			return self.receive( [ 0x80 | (args[ 0 ] << 3 | args[ 1 ] << 1), 0xFF ] )[ 1 ]
-		elif n_args is 3:
-			self.send( [ (args[ 0 ] << 3 | args[ 1 ] << 1), args[ 2 ] ] )
+		if n_args is 1:
+			return super().receive( [ 0x80 | (args[ 0 ] << 3 | self.ch), 0xFF ] )[ 1 ]
+		elif n_args is 2:
+			super().send( [ (args[ 0 ] << 3 | self.ch), args[ 1 ] ] )
 		else:
 			print( "reg_access error" )
 
-	def reg_access_old( self, reg, ch, val ):
-		self.send( [ (reg << 3 | ch << 1), val ] )
+	def tx_buffer_empty( self ):
+		return self.reg_access( self.LSR ) & 0x20
+
+	def wait_tx_ready( self ):
+		while not self.tx_buffer_empty():
+			pass
+
+	def send( self, data ):
+		print( "sending" )
+		if isinstance( data, int ):
+			print( "it's int" )
+			data	= [ data ]
+		elif isinstance( data, str ):
+			print( "it's str" )
+			data	= list( data )
+			data	= [ ord( i ) for i in data ]
+		
+		for d in data:
+			self.wait_tx_ready()		
+			self.reg_access( self.THR, d )
+
+	def receive_check( self ):
+		pass
+		
+	def receive_check2( self ):
+		if ( self.reg_access( self.LSR ) & 0x01 ):
+			print( "got data" )
+		else:
+			print( "no data" )
+		
+		print( self.reg_access( self.RHR ) )
+	
 
 def main():
 	spi	= SPI( 0, 1000 * 1000, cs = 0 )
 	br	= SC16IS752( spi )
-
+	
+	br.baud( 9600 )
+	print( "" )
+	
 	while True:
 		#print( "#" )
-		br.write_registers( br.THR << 3, 0xAA )
-		sleep( .01 )
-		br.write_registers( br.THR << 3, 0x55 )
-		sleep( .01 )
+		br.send( 0xAA )
+		br.receive_check()
+		br.receive_check()
+		br.send( 0x55 )
+		br.send( [ x for x in range( 8 ) ] )
+		br.send( "abcdefg" )
+		br.receive_check()
+		br.receive_check()
 
 if __name__ == "__main__":
 	main()
