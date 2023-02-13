3c3e0f36d8dd4d765cc447e11c7af1550a379850
Tedd OKANO <okano.akifumi@gmail.com>
2023-02-13 13:49:48 +0900

    docs folder added for docstring document generation

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# "mikan" docs folder
+
+This folder help to generate docstring document. 
+HTML documents will be generated in docs/\_build/\*.html after executing following commands in mikan's top folder.  
+
+`$ sphinx-apidoc -f -o ./docs .`
+`$ sphinx-build ./docs ./docs/_build`
---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+from unittest.mock import MagicMock
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+#sys.path.insert(0, os.path.abspath('./'))
+sys.modules['machine'] = MagicMock()
+sys.modules['utime'] = MagicMock()
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'mikan'
+copyright = '2023, Tedd OKANO'
+author = 'Tedd OKANO'
+release = '1.7'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [  'sphinx.ext.autodoc',
+				'sphinx.ext.napoleon',
+				'sphinx.ext.viewcode',
+				'sphinx.ext.doctest',
+				'sphinx.ext.intersphinx',
+				'sphinx.ext.todo',
+				'sphinx.ext.coverage',
+				]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
