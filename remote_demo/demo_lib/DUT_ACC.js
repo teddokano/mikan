36aa68f28f89eaa2d3d03a100af28e34c28a0ed3
Tedd OKANO <okano.akifumi@gmail.com>
2023-03-10 10:25:28 +0900

    making # of graph panes variable

--- remote_demo/demo_lib/DUT_ACC.js
+++ remote_demo/demo_lib/DUT_ACC.js
@@ -104,116 +104,117 @@ window.addEventListener( 'load', function () {
 	setInterval( getDataAndShow, 200 );
 });
 
-let	acc	= {
-	type: 'line',
-	data: {
-		labels: [],
-		datasets: [
-			{
-				label: 'x',
-				data: [],
-				borderColor: "rgba( 255, 0, 0, 1 )",
-				backgroundColor: "rgba( 0, 0, 0, 0 )"
-			},
-			{
-				label: 'y',
-				data: [],
-				borderColor: "rgba( 0, 255, 0, 1 )",
-				backgroundColor: "rgba( 0, 0, 0, 0 )"
-			},
-			{
-				label: 'z',
-				data: [],
-				borderColor: "rgba( 0, 0, 255, 1 )",
-				backgroundColor: "rgba( 0, 0, 0, 0 )"
-			},
-		],
-	},
-	options: {
-		animation: false,
-		title: {
-			display: true,
-			text: '"g" now'
-		},
-		scales: {
-			yAxes: [{
-				ticks: {
-					suggestedMax: 2.0,
-					suggestedMin: -2.0,
-					stepSize: 1,
-					callback: function(value, index, values){
-					return  value +  ' g'
+let	settings	= [
+					{
+						type: 'line',
+						data: {
+							labels: [],
+							datasets: [
+								{
+									label: 'x',
+									data: [],
+									borderColor: "rgba( 255, 0, 0, 1 )",
+									backgroundColor: "rgba( 0, 0, 0, 0 )"
+								},
+								{
+									label: 'y',
+									data: [],
+									borderColor: "rgba( 0, 255, 0, 1 )",
+									backgroundColor: "rgba( 0, 0, 0, 0 )"
+								},
+								{
+									label: 'z',
+									data: [],
+									borderColor: "rgba( 0, 0, 255, 1 )",
+									backgroundColor: "rgba( 0, 0, 0, 0 )"
+								},
+							],
+						},
+						options: {
+							animation: false,
+							title: {
+								display: true,
+								text: '"g" now'
+							},
+							scales: {
+								yAxes: [{
+									ticks: {
+										suggestedMax: 2.0,
+										suggestedMin: -2.0,
+										stepSize: 1,
+										callback: function(value, index, values){
+										return  value +  ' g'
+										}
+									},
+									scaleLabel: {
+										display: true,
+										labelString: ' ]gravitational acceleration [g]'
+									}
+								}],
+								xAxes: [{
+									scaleLabel: {
+										display: true,
+										labelString: 'time'
+									}
+								}]
+							},
+						}
+					},
+					{
+						type: 'line',
+						data: {
+							labels: [],
+							datasets: [
+								{
+									label: 'x',
+									data: [],
+									borderColor: "rgba( 255, 0, 0, 1 )",
+									backgroundColor: "rgba( 0, 0, 0, 0 )"
+								},
+								{
+									label: 'y',
+									data: [],
+									borderColor: "rgba( 0, 255, 0, 1 )",
+									backgroundColor: "rgba( 0, 0, 0, 0 )"
+								},
+								{
+									label: 'z',
+									data: [],
+									borderColor: "rgba( 0, 0, 255, 1 )",
+									backgroundColor: "rgba( 0, 0, 0, 0 )"
+								},
+							],
+						},
+						options: {
+							animation: false,
+							title: {
+								display: true,
+								text: '"mag" now'
+							},
+							scales: {
+								yAxes: [{
+									ticks: {
+										callback: function(value, index, values){
+										return  value +  ' nT'
+										}
+									},
+									scaleLabel: {
+										display: true,
+										labelString: 'geomagnetism [nT]'
+									}
+								}],
+								xAxes: [{
+									scaleLabel: {
+										display: true,
+										labelString: 'time'
+									}
+								}]
+							},
+						}
 					}
-				},
-				scaleLabel: {
-					display: true,
-					labelString: ' ]gravitational acceleration [g]'
-				}
-			}],
-			xAxes: [{
-				scaleLabel: {
-					display: true,
-					labelString: 'time'
-				}
-			}]
-		},
-	}
-};
-
-let	mag	= {
-	type: 'line',
-	data: {
-		labels: [],
-		datasets: [
-			{
-				label: 'x',
-				data: [],
-				borderColor: "rgba( 255, 0, 0, 1 )",
-				backgroundColor: "rgba( 0, 0, 0, 0 )"
-			},
-			{
-				label: 'y',
-				data: [],
-				borderColor: "rgba( 0, 255, 0, 1 )",
-				backgroundColor: "rgba( 0, 0, 0, 0 )"
-			},
-			{
-				label: 'z',
-				data: [],
-				borderColor: "rgba( 0, 0, 255, 1 )",
-				backgroundColor: "rgba( 0, 0, 0, 0 )"
-			},
-		],
-	},
-	options: {
-		animation: false,
-		title: {
-			display: true,
-			text: '"mag" now'
-		},
-		scales: {
-			yAxes: [{
-				ticks: {
-					callback: function(value, index, values){
-					return  value +  ' nT'
-					}
-				},
-				scaleLabel: {
-					display: true,
-					labelString: 'geomagnetism [nT]'
-				}
-			}],
-			xAxes: [{
-				scaleLabel: {
-					display: true,
-					labelString: 'time'
-				}
-			}]
-		},
-	}
-};
+];
 
 let graph	= [];
 
-graph.push( new GraphDraw( SPLIT[ 0 ], acc ) );
-graph.push( new GraphDraw( SPLIT[ 1 ], mag ) );
+graph.push( new GraphDraw( SPLIT[ 0 ], settings[ 0 ] ) );
+graph.push( new GraphDraw( SPLIT[ 1 ], settings[ 1 ] ) );
