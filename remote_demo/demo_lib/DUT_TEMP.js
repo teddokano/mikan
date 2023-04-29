79e7a33b1adb05c288bbd9d2b13c9df54988bc12
Tedd OKANO <okano.akifumi@gmail.com>
2023-04-29 14:41:11 +0900

    gauge disabled on tempsensor page

--- remote_demo/demo_lib/DUT_TEMP.js
+++ remote_demo/demo_lib/DUT_TEMP.js
@@ -1,59 +1,3 @@
-let gauge0 = new JustGage({
-	id: 'gauge0',
-	value: 0,
-	min: -55,
-	max: 125,
-	decimals: 3,
-	symbol: '℃',
-	pointer: true,
-	gaugeWidthScale: 0.5,
-	customSectors: [
-		{
-			color: '#0000ff',
-			lo: -55,
-			hi: 15
-		},
-		{
-			color: '#00ff00',
-			lo: 15,
-			hi: 30
-		},
-		{
-			color: '#ff0000',
-			lo: 30,
-			hi: 125
-		}
-	],
-	counter: false
-});
-
-
-let gauge1 = new JustGage({
-	id: 'gauge1',
-	value: 0,
-	min: 10,
-	max: 30,
-	decimals: 3,
-	symbol: '℃',
-	pointer: true,
-	gaugeWidthScale: 0.5,
-	customSectors: [
-		{
-			color: '#00ff00',
-			lo: 10,
-			hi: 27
-		},
-		{
-			color: '#ff0000',
-			lo: 27,
-			hi: 30
-		}
-	],
-	counter: false
-});
-
-
-
 let	temp_data	= {
 	time:[],
 	temp:[],
@@ -92,8 +36,8 @@ let	temp_data	= {
 				
 				elem.innerText = temp_now.toFixed( 3 ) + '˚C';
 
-				gauge0.refresh( temp_now );
-				gauge1.refresh( temp_now );
+				//gauge0.refresh( temp_now );
+				//gauge1.refresh( temp_now );
 			}
 			
 			for ( let i = 0; i < TABLE_LEN; i++ )
@@ -333,3 +277,61 @@ window.addEventListener( 'load', function () {
 	temp_data.draw();
 	setInterval( getTempAndShow, 1000 );
 });
+
+/*
+ let gauge0 = new JustGage({
+	id: 'gauge0',
+	value: 0,
+	min: -55,
+	max: 125,
+	decimals: 3,
+	symbol: '℃',
+	pointer: true,
+	gaugeWidthScale: 0.5,
+	customSectors: [
+		{
+			color: '#0000ff',
+			lo: -55,
+			hi: 15
+		},
+		{
+			color: '#00ff00',
+			lo: 15,
+			hi: 30
+		},
+		{
+			color: '#ff0000',
+			lo: 30,
+			hi: 125
+		}
+	],
+	counter: false
+});
+
+
+let gauge1 = new JustGage({
+	id: 'gauge1',
+	value: 0,
+	min: 10,
+	max: 30,
+	decimals: 3,
+	symbol: '℃',
+	pointer: true,
+	gaugeWidthScale: 0.5,
+	customSectors: [
+		{
+			color: '#00ff00',
+			lo: 10,
+			hi: 27
+		},
+		{
+			color: '#ff0000',
+			lo: 27,
+			hi: 30
+		}
+	],
+	counter: false
+});
+*/
+
+
