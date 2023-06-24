ac12cc63d633177e89da0d38da49df3292e3ecce
Tedd OKANO <okano.akifumi@gmail.com>
2023-06-25 05:20:52 +0900

    mikan-interface library compatible version. / channel initialization improved

--- nxp_periph/afe.py
+++ nxp_periph/afe.py
@@ -13,10 +13,8 @@ class AFE_base:
 class NAFE13388( AFE_base, SPI_target ):
 	def __init__( self, spi, cs = None ):
 		SPI_target.__init__( self, spi, cs )
-		self.spi	= spi
+#		self.spi	= spi
 
-		self.count	= 0
-		
 		self.reset_pin	= Pin( "D6", Pin.OUT )
 		self.syn_pin	= Pin( "D5", Pin.OUT )
 		self.drdy_pin	= Pin( "D3", Pin.IN )
@@ -26,10 +24,8 @@ class NAFE13388( AFE_base, SPI_target ):
 		self.syn_pin.value( 1 )
 		
 		self.logical_channel	= [
-#									AFE_LogicalChannel( self, 0, [ 0x1110, 0x0040, 0x1400, 0x0000, 0x0001 ] ),
-#									AFE_LogicalChannel( self, 1, [ 0x3350, 0x0040, 0x4100, 0x3060, 0x0002 ] ),
-									AFE_LogicalChannel( self, 0, [ 0x11F0, 0x0040, 0x1400, 0x0000, 0x0001 ] ),
-									AFE_LogicalChannel( self, 1, [ 0x33F0, 0x0040, 0x4100, 0x3060, 0x0002 ] ),
+									AFE_LogicalChannel( self, 0, [ 0x11F0, 0x0040, 0x1400, 0x0000 ] ),
+									AFE_LogicalChannel( self, 1, [ 0x33F0, 0x0040, 0x4100, 0x3060 ] ),
 									]
 		
 		self.weight_offset	= -172.7
@@ -44,7 +40,7 @@ class NAFE13388( AFE_base, SPI_target ):
 			tim0	= machine.Timer( 0 )
 			tim0.init( period = int( sampling_interval * 1000.0 ), callback = self.tim_cb )
 		"""
-	def	write_reg( self, reg, val = None ):
+	def	write_r16( self, reg, val = None ):
 		reg		<<= 1
 	
 		regH	= reg >> 8 & 0xFF
@@ -53,29 +49,29 @@ class NAFE13388( AFE_base, SPI_target ):
 		if val:
 			valH	= val >> 8 & 0xFF
 			valL	= val & 0xFF
-			self.spi.write( bytearray( [ regH, regL, valH, valL ] ) )
+			self.send( [ regH, regL, valH, valL ] )
 		else:
-			self.spi.write( bytearray( [ regH, regL ] ) )
+			self.send( [ regH, regL ] )
 
-	def	read_reg( self, reg ):
+	def	read_r16( self, reg ):
 		reg		<<= 1
 		reg		|= 0x4000
 		regH	= reg >> 8 & 0xFF
 		regL	= reg & 0xFF
 
 		data	= bytearray( [ regH, regL, 0xFF, 0xFF ] )
-		self.spi.write_readinto( data, data )
-
+		self.__if.write_readinto( data, data )
+		
 		return unpack( ">H", data[2:] )[ 0 ]
 
-	def	read_data( self, reg ):
+	def	read_r24( self, reg ):
 		reg		<<= 1
 		reg		|= 0x4000
 		regH	= reg >> 8 & 0xFF
 		regL	= reg & 0xFF
 
 		data	= bytearray( [ regH, regL, 0xFF, 0xFF, 0xFF ] )
-		self.spi.write_readinto( data, data )
+		self.__if.write_readinto( data, data )
 
 		data	+= b'\x00'		
 		data	= unpack( ">l", data[2:] )[ 0 ] >> 8
@@ -83,24 +79,31 @@ class NAFE13388( AFE_base, SPI_target ):
 		return data
 
 	def boot( self ):
-		self.write_reg( 0x0010 )
-		self.write_reg( 0x002A, 0x0000 )
-		self.write_reg( 0x002B, 0x0000 )
-		self.write_reg( 0x002C, 0x0000 )
-		self.write_reg( 0x002F, 0x0000 )
-		self.write_reg( 0x0029, 0x0000 )
-		sleep( 0.001 )
-		self.write_reg( 0x0030, 0x0010 )
-		sleep( 0.001 )
-
+		reg_init	= [
+						{	0x0010: None,
+							0x002A: 0x0000,
+							0x002B: 0x0000,
+							0x002C: 0x0000,
+							0x002F: 0x0000,
+							0x0029: 0x0000,
+							},
+					   {	0x0030: 0x0010, 
+							},
+					]
+					
+		for step in reg_init:
+			for k, v in step.items():
+				self.write_r16( k, v )
+			sleep( 0.001 )
+		
 	def reset( self ):
-		self.write_reg( 0x0014 )
+		self.write_r16( 0x0014 )
 		sleep( 0.001 )
 
 	def dump( self, list ):
 		for r in list:
 			if r:
-				print( "0x{:04X} = {:04X}".format( r, self.read_reg( r ) ) )
+				print( "0x{:04X} = {:04X}".format( r, self.read_r16( r ) ) )
 			else:
 				print( "" )
 
@@ -131,14 +134,14 @@ class AFE_LogicalChannel:
 		self.cnfg	= list
 		
 	def read( self ):
-		self.afe.write_reg( 0x0000 + self.ch )
+		self.afe.write_r16( 0x0000 + self.ch )
 		
 		for r, v in zip( self.rlist, self.cnfg ):
-			self.afe.write_reg( r, v )
+			self.afe.write_r16( r, v )
 		
-		self.afe.write_reg( 0x2000 )
+		self.afe.write_r16( 0x2000 )
 		sleep( 0.001 )
-		return self.afe.read_data( 0x2040 + self.ch )
+		return self.afe.read_r24( 0x2040 + self.ch )
 
 def main():
 	spi	= SPI( 0, 1000_000, cs = 0, phase = 1 )
@@ -150,12 +153,12 @@ def main():
 	afe.dump( [ 0x7C, 0x7D, 0x7E, 0xAE, 0xAF, 0x34, 0x37, None, 0x30, 0x31 ] )
 
 	print( "ch 0" )
-	afe.write_reg( 0x0000 )
+	afe.write_r16( 0x0000 )
 	afe.dump( [ 0x20, 0x21, 0x22, 0x23 ] )
 
 	print( "ch 1" )
-	afe.write_reg( 0x0001 )
-	afe.dump( [ 0x20, 0x21, 0x22, 0x23, 0x24 ] )
+	afe.write_r16( 0x0001 )
+	afe.dump( [ 0x20, 0x21, 0x22, 0x23 ] )
 	
 	count	= 0
 	
