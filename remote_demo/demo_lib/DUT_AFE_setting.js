c8da4734e8425353323ad650298a787d9efdffe9
Tedd OKANO <okano.akifumi@gmail.com>
2023-08-05 10:11:07 +0900

    adding setting panel

---
+++ remote_demo/demo_lib/DUT_AFE_setting.js
@@ -0,0 +1,11 @@
+function show_setting_panel() {
+	document.getElementById('show_button').style.display = 'none';
+	document.getElementById('hide_button').style.display = 'block';
+	document.getElementById('setting').style.display = 'block';
+}
+
+function hide_setting_panel() {
+	document.getElementById('show_button').style.display = 'block';
+	document.getElementById('hide_button').style.display = 'none';
+	document.getElementById('setting').style.display = 'none';
+}
