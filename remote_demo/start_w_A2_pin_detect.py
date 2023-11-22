a56a2fbdc0e3a66cc0481d292e40b6fbb8a47c0f
Tedd OKANO <okano.akifumi@gmail.com>
2023-11-23 08:47:56 +0900

    remote_demo jumper pin boot option

---
+++ remote_demo/start_w_A2_pin_detect.py
@@ -0,0 +1,19 @@
+from machine  import Pin
+from demo_lib import demo
+from utime    import sleep
+
+def main():
+	setting_detect	= Pin( "A2", Pin.IN )
+	
+	if setting_detect.value():
+		demo()
+	else:
+		demo( ip = (	"10.0.0.99", 		#	IP address
+						"255.255.255.0", 	#	Subnet mask
+						"10.0.0.1", 		#	Gateway
+						"0.0.0.0" 			#	DNS
+						)
+					)
+
+if __name__ == "__main__":
+	main()
