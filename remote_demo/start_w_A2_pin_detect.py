f918804c9871a55ab439b1f9df8ab7aeb3c0fea4
Tedd OKANO <okano.akifumi@gmail.com>
2024-10-02 16:00:37 +0900

    remote_demo is modified to use fixed IP address when A2 pin is HIGH

--- remote_demo/start_w_A2_pin_detect.py
+++ remote_demo/start_w_A2_pin_detect.py
@@ -6,7 +6,13 @@ def main():
 	setting_detect	= Pin( "A2", Pin.IN )
 	
 	if setting_detect.value():
-		demo()
+		#demo()		### modified to use fixed IP when A2 pin is HIGH
+		demo( ip = (	"10.0.1.2", 		#	IP address
+						"255.255.255.0", 	#	Subnet mask
+						"10.0.0.1", 		#	Gateway
+						"0.0.0.0" 			#	DNS
+						)
+					)
 	else:
 		demo( ip = (	"10.0.0.99", 		#	IP address
 						"255.255.255.0", 	#	Subnet mask
